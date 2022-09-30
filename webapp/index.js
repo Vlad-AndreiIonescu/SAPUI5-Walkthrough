5220b0962006d0a3d3196a61be75c3cbf80c7449
Vlad Ionescu <vlad.ionescu@bearingpoint.com>
2022-09-30 14:36:18 +0300

    3rd step done

--- webapp/index.js
+++ webapp/index.js
@@ -1,7 +1,9 @@
 sap.ui.define([
-
-], function(){
+    "sap/m/Text" //sap/m -> control library .. /Text -> control name
+], function (Text) {
     "use strict";
 
-    alert("UI5 is ready");
+    new Text({  //cream un ob pt control, setam at text cu val hw
+        text:"Hello World"
+    }).placeAt("content");// face legatura cu id ul din html si l pune acolo
 })
