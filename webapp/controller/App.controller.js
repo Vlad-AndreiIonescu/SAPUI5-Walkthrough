b3d03612e628529a9383bf99e410a6e859c3495a
Vlad Ionescu <vlad.ionescu@bearingpoint.com>
2022-10-03 15:22:23 +0300

    step9

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,34 +1,18 @@
 sap.ui.define([
    "sap/ui/core/mvc/Controller",
-   "sap/m/MessageToast",
-   "sap/ui/model/json/JSONModel",
-   "sap/ui/model/resource/ResourceModel"//nou modul
-], function (Controller, MessageToast, JSONModel, ResourceModel) {
+   "sap/m/MessageToast"
+], function (Controller, MessageToast) {
    "use strict";
    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-     onInit : function () {
-         // set data model on view
-         var oData = {
-            recipient : {
-               name : "World"
-            }
-         };
-         var oModel = new JSONModel(oData);
-         this.getView().setModel(oModel);
-         // set i18n model on view
-         //Primul lucru pe care trb sa l facem e sa instantiem si sa cream modelul
-         var i18nModel = new ResourceModel({
-            bundleName: "sap.ui.demo.walkthrough.i18n.i18n"// folder.fisier
-         });
-         this.getView().setModel(i18nModel, "i18n");
-      },
       onShowHello : function () {
          // read msg from i18n model
          var oBundle = this.getView().getModel("i18n").getResourceBundle();
-         var sRecipient = this.getView().getModel().getProperty("/recipient/name");// de sus de la obiectul oData
+         var sRecipient = this.getView().getModel().getProperty("/recipient/name");
          var sMsg = oBundle.getText("helloMsg", [sRecipient]);
          // show message
-         MessageToast.show(sMsg);//afisam  variabila creata mai sus 
+         MessageToast.show(sMsg);
       }
    });
 });
+
+// am scos onInit function si dependintele  respective pt ca sunt deja in componenta
--- webapp/index.js
+++ webapp/index.js
@@ -1,14 +1,15 @@
-
-
 sap.ui.define([
-	"sap/ui/core/mvc/XMLView"// definim XMLVIEW
-], function (XMLView) {//pasam ob XMLView in callbackfunction
+	"sap/ui/core/ComponentContainer"
+], function (ComponentContainer) {
 	"use strict";
 
-	XMLView.create({//cream un nou view
-		viewName: "sap.ui.demo.walkthrough.view.App"//avem nev de viewName care e locatia xml ului nostru
-	}).then(function (oView) {
-		oView.placeAt("content");//plasam view-ul nostru in body prin content
-	});
-
+	new ComponentContainer({
+		name: "sap.ui.demo.walkthrough",
+		settings : {
+			id : "walkthrough"
+		},
+		async: true
+	}).placeAt("content");
 });
+
+//acum instantiem o componenta si nu mai folosim view ca inainte
