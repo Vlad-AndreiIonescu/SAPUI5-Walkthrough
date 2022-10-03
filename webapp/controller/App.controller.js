005f95f1aa7b2579daf03d472866ca94d7e02172
Vlad Ionescu <vlad.ionescu@bearingpoint.com>
2022-10-03 11:59:32 +0300

    step8

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,25 +1,34 @@
 sap.ui.define([
    "sap/ui/core/mvc/Controller",
    "sap/m/MessageToast",
-   "sap/ui/model/json/JSONModel"
-], function (Controller, MessageToast, JSONModel) {
+   "sap/ui/model/json/JSONModel",
+   "sap/ui/model/resource/ResourceModel"//nou modul
+], function (Controller, MessageToast, JSONModel, ResourceModel) {
    "use strict";
    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-      onInit : function () {// metoda onInit e declansata prima data cand controller ul e creat
+     onInit : function () {
          // set data model on view
-         var oData = {// creare data object cu proprietatile..
+         var oData = {
             recipient : {
                name : "World"
             }
          };
-         var oModel = new JSONModel(oData);//instantiere, creare JSONModel, punem ob ca param
-         
-         this.getView().setModel(oModel);//ca sa putem folosi JSONModel in view ul nostru
-                                          //this.getView()-> ne ia viewul nostru 
-                                          //si cu set ne setam jsonModelul nostru
+         var oModel = new JSONModel(oData);
+         this.getView().setModel(oModel);
+         // set i18n model on view
+         //Primul lucru pe care trb sa l facem e sa instantiem si sa cream modelul
+         var i18nModel = new ResourceModel({
+            bundleName: "sap.ui.demo.walkthrough.i18n.i18n"// folder.fisier
+         });
+         this.getView().setModel(i18nModel, "i18n");
       },
       onShowHello : function () {
-         MessageToast.show("Hello World");
+         // read msg from i18n model
+         var oBundle = this.getView().getModel("i18n").getResourceBundle();
+         var sRecipient = this.getView().getModel().getProperty("/recipient/name");// de sus de la obiectul oData
+         var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+         // show message
+         MessageToast.show(sMsg);//afisam  variabila creata mai sus 
       }
    });
 });
