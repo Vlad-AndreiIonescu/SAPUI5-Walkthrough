c02e5a173820c5463b5c9e4cd2a85919b72510bd
Vlad Ionescu <vlad.ionescu@bearingpoint.com>
2022-10-03 11:17:08 +0300

    step 7

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,24 @@
-sap.ui.define([//incarcarea este always async
-   "sap/ui/core/mvc/Controller", //si controller si messageToast sun dependinte
-   "sap/m/MessageToast"
-], function (Controller, MessageToast) {//o sa se declanseze callback f si pasam ob celor doua module
+sap.ui.define([
+   "sap/ui/core/mvc/Controller",
+   "sap/m/MessageToast",
+   "sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, JSONModel) {
    "use strict";
-   //abia dupa ce modulele sunt incarcate cu succes, codul nostru se executa
    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-      onShowHello: function () {
+      onInit : function () {// metoda onInit e declansata prima data cand controller ul e creat
+         // set data model on view
+         var oData = {// creare data object cu proprietatile..
+            recipient : {
+               name : "World"
+            }
+         };
+         var oModel = new JSONModel(oData);//instantiere, creare JSONModel, punem ob ca param
+         
+         this.getView().setModel(oModel);//ca sa putem folosi JSONModel in view ul nostru
+                                          //this.getView()-> ne ia viewul nostru 
+                                          //si cu set ne setam jsonModelul nostru
+      },
+      onShowHello : function () {
          MessageToast.show("Hello World");
       }
    });
