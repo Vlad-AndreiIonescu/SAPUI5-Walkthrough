3191accc1a4b04cd8c3dc25f278944bd012d6986
Vlad Ionescu <vlad.ionescu@bearingpoint.com>
2022-10-03 10:40:07 +0300

    step 6

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,12 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"//definim controller ul
- ], function (Controller) {
-    "use strict";
-    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-       onShowHello : function () {//event care va face o alerta. E folosir in view la apasare.
-          // show a native JavaScript alert
-          alert("Hello World");
-       }
-    });
- });
+sap.ui.define([//incarcarea este always async
+   "sap/ui/core/mvc/Controller", //si controller si messageToast sun dependinte
+   "sap/m/MessageToast"
+], function (Controller, MessageToast) {//o sa se declanseze callback f si pasam ob celor doua module
+   "use strict";
+   //abia dupa ce modulele sunt incarcate cu succes, codul nostru se executa
+   return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+      onShowHello: function () {
+         MessageToast.show("Hello World");
+      }
+   });
+});
