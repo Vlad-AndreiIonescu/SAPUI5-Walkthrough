e180d9795de053d710f22e86617057547276cfd3
Vlad Ionescu <vlad.ionescu@bearingpoint.com>
2022-10-04 12:21:58 +0300

    step17

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,31 +1,39 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/core/Fragment"
- ], function (Controller, MessageToast, Fragment) {
-    "use strict";
-    return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
-       onShowHello : function () {
-          // read msg from i18n model
-          var oBundle = this.getView().getModel("i18n").getResourceBundle();
-          var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-          var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-          // show message
-          MessageToast.show(sMsg);
-       },
-       
-		onOpenDialog : function () {//noul event de deschidere a dialog-ului
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast",
+	"sap/ui/core/Fragment"
+], function (Controller, MessageToast, Fragment) {
+	"use strict";
 
-			// create dialog lazily
-            //DUPA THIS POT SA SCRIU ORICE
-			if (!this.pDial) {//daca dialogul nu exista, sa l creeze
-				this.pDial = this.loadFragment({
+	return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
+
+		onShowHello : function () {
+			// read msg from i18n model
+			var oBundle = this.getView().getModel("i18n").getResourceBundle();
+			var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
+			// show message
+			MessageToast.show(sMsg);
+		},
+
+		onOpenDialog : function () {
+
+			if (!this.pDialog) {
+				this.pDialog = this.loadFragment({
 					name: "sap.ui.demo.walkthrough.view.HelloDialog"
 				});
 			} 
-			this.pDial.then(function(oDialog) {//daca exista sa l deschida
+			this.pDialog.then(function(oDialog) {
 				oDialog.open();
 			});
+		},
+
+		onCloseDialog : function () {
+			// note: We don't need to chain to the pDialog promise, since this event-handler
+			// is only called from within the loaded dialog itself.
+			this.byId("helloDialog").close();
 		}
-    });
- });
+	});
+
+});
