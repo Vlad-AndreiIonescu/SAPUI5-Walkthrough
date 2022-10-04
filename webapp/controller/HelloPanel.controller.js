695f2a007ff768577772c629b4766b9033c2ea5b
Vlad Ionescu <vlad.ionescu@bearingpoint.com>
2022-10-04 12:11:22 +0300

    step16

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
- ], function (Controller, MessageToast) {
+    "sap/m/MessageToast",
+    "sap/ui/core/Fragment"
+ ], function (Controller, MessageToast, Fragment) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
        onShowHello : function () {
@@ -11,6 +12,20 @@ sap.ui.define([
           var sMsg = oBundle.getText("helloMsg", [sRecipient]);
           // show message
           MessageToast.show(sMsg);
-       }
+       },
+       
+		onOpenDialog : function () {//noul event de deschidere a dialog-ului
+
+			// create dialog lazily
+            //DUPA THIS POT SA SCRIU ORICE
+			if (!this.pDial) {//daca dialogul nu exista, sa l creeze
+				this.pDial = this.loadFragment({
+					name: "sap.ui.demo.walkthrough.view.HelloDialog"
+				});
+			} 
+			this.pDial.then(function(oDialog) {//daca exista sa l deschida
+				oDialog.open();
+			});
+		}
     });
  });
